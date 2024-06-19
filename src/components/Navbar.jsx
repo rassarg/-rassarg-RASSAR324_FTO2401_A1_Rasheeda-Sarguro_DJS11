f18b9d0a912096317c1f018564ab212a2516142f
rasheeda <raskieda@gmail.com>
2024-06-19 12:09:08 -0400

    updated Navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,19 @@ import "./Navbar.css";
 
 const Navbar = () => (
   <div className="navbar">
-    <Link to="/" className="nav-link">
-      <FaHome />
-      <span className="tooltip">Home</span>
-    </Link>
-    <h1>✨ PODCAST PLAYGROUND ✨</h1>
-    <Link to="/favourites" className="nav-link">
-      <FaStar />
-      <span className="tooltip">Favourites</span>
-    </Link>
+    <div className="navbar-title">
+      <h1>✨ PODCAST PLAYGROUND ✨</h1>
+    </div>
+    <div className="nav-icons">
+      <Link to="/" className="nav-link">
+        <FaHome size={24} /> {/* Increase the size as needed */}
+        <span className="tooltip"> Home</span>
+      </Link>
+      <Link to="/favourites" className="nav-link">
+        <FaStar size={24} /> {/* Increase the size as needed */}
+        <span className="tooltip">Favourites</span>
+      </Link>
+    </div>
   </div>
 );
 

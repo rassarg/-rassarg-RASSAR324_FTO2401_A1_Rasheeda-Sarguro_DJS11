d9f7fc561f9fe5e586d7e398b4af6554379c4ce1
rasheeda <raskieda@gmail.com>
2024-06-19 20:12:02 -0400

    added a Back to show button and rendered season view

--- src/pages/ShowDetail.jsx
+++ src/pages/ShowDetail.jsx
@@ -7,11 +7,11 @@ import "./ShowDetail.css";
 
 const ShowDetail = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null); // state for the show data
+  const [show, setShow] = useState(null);
   const [selectedSeason, setSelectedSeason] = useState(null);
   const [loading, setLoading] = useState(true);
-  const { playEpisode } = usePlayer(); // Custom hook to access player functionality
-  const [clickedButton, setClickedButton] = useState(null); // state to track clicked button
+  const { playEpisode } = usePlayer();
+  const [clickedButton, setClickedButton] = useState(null);
 
   // Fetch show data when component mounts or 'id' parameter changes
   useEffect(() => {
@@ -19,7 +19,6 @@ const ShowDetail = () => {
       try {
         const data = await fetchShowById(id);
         setShow(data);
-        setSelectedSeason(data.seasons[0]); // Select the first season by default
         setLoading(false);
       } catch (error) {
         console.error("Error fetching show details:", error);
@@ -29,13 +28,12 @@ const ShowDetail = () => {
     fetchData();
   }, [id]);
 
-  // Handler function to change the selected season
+  // Handle season change
   const handleSeasonChange = (season) => {
     setSelectedSeason(season);
     setClickedButton(null);
   };
-
-  // Handler function to handle click on the "Play" button
+  // Handle episode button click
   const handleButtonClick = (episode, show, season, index) => {
     playEpisode(episode, show, season);
     setClickedButton(index);
@@ -50,54 +48,69 @@ const ShowDetail = () => {
 
   return (
     <div className="show-details">
-      <h1 className="show-title">{show.title}</h1>
-      <div className="show-info">
-        <div className="image-container">
-          {selectedSeason && (
-            <img
-              src={selectedSeason.image}
-              alt={`Season ${selectedSeason.season}`}
-              className="season-image"
-            />
-          )}
-        </div>
-        <p className="show-description">{show.description}</p>
-      </div>
-      <div className="season-selector">
-        {show.seasons.map((season, index) => (
+      {selectedSeason ? (
+        <div>
           <button
-            key={`${show.id}-${season.season}`}
-            onClick={() => handleSeasonChange(season)}
-            className={season === selectedSeason ? "active" : "inactive"}
-            disabled={season === selectedSeason}
+            className="back-button"
+            onClick={() => setSelectedSeason(null)}
           >
-            Season {season.season}{" "}
-            {season === selectedSeason && "(currently selected)"}
+            &#8592; Back to Show
           </button>
-        ))}
-      </div>
-      <div className="episode-list">
-        <h2 className="episodes-heading">Episodes:</h2>
-        {selectedSeason.episodes.map((episode, index) => (
-          <div key={`${selectedSeason.season}-${index}`} className="episode">
-            <h3>
-              {episode.episode}. {episode.title}
-            </h3>
-            <button
-              className={`episode-play-button ${
-                clickedButton === index ? "clicked" : ""
-              }`}
-              onClick={() =>
-                handleButtonClick(episode, show, selectedSeason, index)
-              }
-            >
-              Play
-            </button>
+          <h1 className="show-title">{show.title}</h1>
+          <div className="image-container">
+            <img src={show.image} alt={show.title} className="show-image" />
           </div>
-        ))}
-      </div>
+          <div className="show-info"></div>
+          <div className="episode-list">
+            <h2 className="episodes-heading">
+              Episodes in Season {selectedSeason.season}:
+            </h2>
+            {selectedSeason.episodes.map((episode, index) => (
+              <div
+                key={`${selectedSeason.season}-${index}`}
+                className="episode"
+              >
+                <h3>
+                  {episode.episode}. {episode.title}
+                </h3>
+                <button
+                  className={`episode-play-button ${
+                    clickedButton === index ? "clicked" : ""
+                  }`}
+                  onClick={() =>
+                    handleButtonClick(episode, show, selectedSeason, index)
+                  }
+                >
+                  Play
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1 className="show-title">{show.title}</h1>
+          <div className="image-container">
+            <img src={show.image} alt={show.title} className="show-image" />
+          </div>
+          <div className="show-info">
+            <p className="show-description">{show.description}</p>
+          </div>
+          <div className="season-selector">
+            <h2 className="seasons-heading">Seasons:</h2>
+            {show.seasons.map((season, index) => (
+              <button
+                key={`${show.id}-${season.season}`}
+                onClick={() => handleSeasonChange(season)}
+                className="season-button"
+              >
+                Season {season.season} ({season.episodes.length} episodes)
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
-
 export default ShowDetail;

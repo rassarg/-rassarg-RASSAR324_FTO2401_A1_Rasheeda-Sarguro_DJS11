18a47a2ee7aecbc51a91f97c9d3c1b0d6b441119
rasheeda <raskieda@gmail.com>
2024-06-20 14:58:25 -0400

    added showNotification

--- src/components/FavouriteButton.jsx
+++ src/components/FavouriteButton.jsx
@@ -1,31 +1,58 @@
-// FavouriteButton.jsx
 import React, { useState, useEffect } from "react";
+import "./FavouriteButton.css";
 
 const FavouriteButton = ({ onToggle, uniqueId }) => {
   const [isFavourite, setIsFavourite] = useState(
     localStorage.getItem(`favourite-${uniqueId}`) === "true"
   );
+  const [showNotification, setShowNotification] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState("");
 
   useEffect(() => {
     localStorage.setItem(`favourite-${uniqueId}`, isFavourite);
+
+    if (isFavourite) {
+      const hasShownNotification = localStorage.getItem(
+        `notification-shown-${uniqueId}`
+      );
+      if (!hasShownNotification) {
+        setNotificationMessage("Added to favourites!");
+        setShowNotification(true);
+        localStorage.setItem(`notification-shown-${uniqueId}`, "true");
+        setTimeout(() => {
+          setShowNotification(false);
+        }, 1000); // Show notification for 1 second
+        console.log("Added to favourites:", uniqueId);
+      }
+    }
   }, [isFavourite, uniqueId]);
 
   const toggleFavourite = () => {
     setIsFavourite(!isFavourite);
     onToggle();
-    console.log(
-      `Episode ${uniqueId} ${
-        isFavourite ? "removed from" : "added to"
-      } favorites`
-    );
+
+    if (isFavourite) {
+      setNotificationMessage("Removed from favourites!");
+      console.log("Removed from favourites:", uniqueId);
+    } else {
+      setNotificationMessage("Added to favourites!");
+      console.log("Added to favourites:", uniqueId);
+    }
+    setShowNotification(true);
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 1000); // Show notification for 1 second
   };
 
   return (
     <span
+      className={`favourite-button ${isFavourite ? "gold" : ""}`}
       onClick={toggleFavourite}
-      style={{ cursor: "pointer", color: isFavourite ? "gold" : "black" }}
     >
       &#9734;
+      <span className={`notification ${showNotification ? "show" : ""}`}>
+        {notificationMessage}
+      </span>
     </span>
   );
 };
